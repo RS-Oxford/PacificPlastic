a9147d2d286a8bc53d2c88ccd61dd2b2509d78ca
ray <rui.song90@gmail.com>
2024-01-17 15:04:16 +0000

    test reading CEDA CDNC datasets

---
+++ InitialTest/Signature_CDNC/read_CDNC.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Filename:    read_CDNC.py
+# @Author:      Dr. Rui Song
+# @Email:       rui.song@physics.ox.ac.uk
+# @Time:        17/01/2024 15:03
